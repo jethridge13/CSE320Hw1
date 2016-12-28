f9aa6bbb38fa1b93435989c34ae17ca18f908ed7
jethridge <jethridge@cs.stonybrook.edu>
2016-12-27 22:03:42 -0500

    Preparation for adding to github

--- hw1.c
+++ hw1.c
@@ -1,20 +1,79 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include "hw1.h"
 
 int main(int argc, char *argv[]) {
-	printf("%d\n", argc);
-	int i = 0;
-	for(i; i < argc; i++){
-		printf("%d\n", *argv[i]);
+	// If more than 3 arguments, doesn't work. 
+	if(argc > 3){
+		fprintf(stderr, "Too many arguments given.\n");
+		printMenu();
+		return EXIT_FAILURE;
 	}
-	printf("Usage:		./mstat [OPTION]\n");
-	printf("		./mstat -h		Displays this help menu.\n");
-	printf("		./mstat -i [-u]	Displays statistics about instruction types.\n");
-	printf("		./mstat -r [-u]	Displays information about the registers.\n");
-	printf("		./mstat -o [-u]	Displays number and percentage of opcodes used.\n");
-	printf("\n");
-	printf("Optional flags:\n");
-	printf("-u 			Displays human readable headers for the different outputs.\n");
-	return EXIT_SUCCESS;
+	if(argc == 1){
+		fprintf(stderr, "No arguments found.\n");
+		printMenu();
+		return EXIT_FAILURE;
+	} else {
+		FILE *fp;
+		fp = stdin;
+		int i = 1;
+		for(; i < argc; i++){
+			if(argv[i][0] == '-'){
+				switch(argv[i][1]){
+					case 'h':
+						printMenu();
+						return EXIT_SUCCESS;
+					case 'u':
+						fprintf(stderr, "The -u argument should only be used with another command.\n");
+						printMenu();
+						return EXIT_FAILURE;
+					case 'i':
+						if(argc - i > 1){
+							if(argv[i+1][0] == '-' && argv[i+1][1] == 'u'){
+								i++;
+								return dispStat(fp, 1);
+							} else {
+								return dispStat(fp, 0);
+							}
+						} else {
+							return dispStat(fp, 0);
+						}
+					case 'r':
+						if(argc - i > 1){
+							if(argv[i+1][0] == '-' && argv[i+1][1] == 'u'){
+								i++;
+								return dispInfo(fp, 1);
+							} else { 
+								return dispInfo(fp, 0);
+							}
+						} else {
+							return dispInfo(fp, 0);
+						}
+					case 'o':
+						if(argc - i > 1){
+							if(argv[i+1][0] == '-' && argv[i+1][1] == 'u'){
+								i++;
+								return dispNum(fp, 1);
+							} else { 
+								return dispNum(fp, 0);
+							}
+						} else {
+							return dispNum(fp, 0);
+						}
+						break;
+					default:
+						fprintf(stderr, "No valid arguments found.\n");
+						printMenu();
+						return EXIT_FAILURE;
+						break;
+				}
+			} else {
+				fprintf(stderr, "Invalid argument found.\n");
+				printMenu();
+				return EXIT_FAILURE;
+			}
+		}
+	}
 }
